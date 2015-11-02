fb091b8e8b0e1de5c2b3ece48ab3aa98f81a23b5
Mark Greene <mark.b.greene@gmail.com>
2015-11-02 08:19:57 -0500

    Hello Phoenix

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,39 @@
+defmodule HelloPhoenix.SessionController do
+  use HelloPhoenix.Web, :controller
+
+  alias HelloPhoenix.User
+
+  import Comeonin.Bcrypt, only: [checkpw: 2]
+
+  def new(conn, _params) do
+    render conn, "new.html", changeset: User.changeset(%User{})
+  end
+
+  plug :scrub_params, "user" when action in [:create]
+
+  def create(conn, %{"user" => user_params}) do
+    user = Repo.get_by(User, username: user_params["username"])
+    user
+    |> sign_in(user_params["password"], conn)
+  end
+
+
+  defp sign_in(user, password, conn) when is_nil(user) do
+      conn
+      |> put_flash(:error, "Invalid username/password combination!")
+      |> redirect(to: page_path(conn, :index))
+  end
+  defp sign_in(user, password, conn) do
+    if checkpw(password, user.password_digest) do
+      conn
+      |> put_session(:current_user, %{id: user.id, username: user.username})
+      |> put_flash(:info, "Sign in successful!")
+      |> redirect(to: page_path(conn, :index))
+    else
+      conn
+      |> put_session(:current_user, nil)
+      |> put_flash(:error, "Invalid username/password combination!")
+      |> redirect(to: page_path(conn, :index))
+    end
+  end
+end
